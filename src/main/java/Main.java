aa5cb1ea5734e73052ca906097bc5bc1b40fbdc8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-18 12:27:32 -0300

    Main - Calling bot sucessfull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import models.FileReader;
+import models.ListenerCommands;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+
 /**
  * Main class, the one that calls the bot and make it run.
  *
@@ -16,7 +22,17 @@ Version 0.:
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        FileReader fileReader = new FileReader("discordtoken");
+
+        DiscordApi api = new DiscordApiBuilder().setToken(fileReader.getFileContent())
+                .setIntents(Intent.GUILDS, Intent.GUILD_MESSAGES, Intent.DIRECT_MESSAGES)
+                .login().join();
+        System.out.println(api.createBotInvite());
+
+        api.addListener(new ListenerCommands());
+
+
     }
 
 }
