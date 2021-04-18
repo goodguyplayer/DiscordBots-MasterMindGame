ed4ff6c501f11df1242ed40c3c5a7abc5cf73fb5
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-18 11:20:26 -0300

    class FileReader created

---
+++ src/main/java/models/FileReader.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Class that loads a file and return the data.
+ *
+ * @author Nathan (goodguyplayer)
+ * @Version 0
+ * @since 2021-04-18
+ */
+public class FileReader {
+}
