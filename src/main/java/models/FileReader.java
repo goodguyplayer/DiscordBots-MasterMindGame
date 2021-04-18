44c867d840f4cedbc106debced9cd9b395db1bde
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-18 11:34:31 -0300

    class FileReader updated - Version 0
    Version 0.:
        - Added vars
        - Added method loadFile
        - Added method getFile
        - Added method getFileContent

--- src/main/java/models/FileReader.java
+++ src/main/java/models/FileReader.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Class that loads a file and return the data.
  *
@@ -7,5 +10,72 @@ package models;
  * @Version 0
  * @since 2021-04-18
  */
+
+/*
+Changelog.:
+
+Version 0.:
+    - Added vars
+    - Added method loadFile
+    - Added method getFile
+    - Added method getFileContent
+
+ */
 public class FileReader {
+    String filename;
+    File file;
+
+    /**
+     * Constructor. Initializes with the filename and starts a new file to read
+     * @param filename name of file
+     * @author Nathan (goodguyplayer)
+     */
+    public FileReader(String filename) {
+        this.filename = filename;
+        loadFile();
+    }
+
+    /**
+     * private method made to load file.
+     * In case there's an error, print error.
+     * @author Nathan (goodguyplayer)
+     */
+    private void loadFile() {
+        try {
+            this.file = new File(this.filename);
+        } catch (Exception e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * gets the file object
+     * @return file object
+     * @author Nathan (goodguyplayer)
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * gets the content of the file.
+     * @return content of file
+     * @author Nathan (goodguyplayer)
+     */
+    public String getFileContent() {
+        try {
+            Scanner myReader = new Scanner(this.file);
+            String data = new String();
+            while (myReader.hasNextLine()) {
+                data += myReader.nextLine();
+            }
+            myReader.close();
+            return data;
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
